33316eda08569b9873c373c1d4f99ef163e455d7
Madhuri <harshupatil0301@gmail>
2021-05-28 09:10:22 -0700

    UC1-LineComparison

--- LineComparison/Line.cs
+++ LineComparison/Line.cs
@@ -6,6 +6,10 @@ namespace LineComparison
 {
     class Line
     {
-        public LengthOfLine(d)
+        public void LengthOfLine(double x1, double y1, double x2, double y2)
+        {
+            double Distance = (Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine(Distance);
+        }
     }
 }
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,6 +7,10 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Line Comparison Program");
-        }       
+            Line line1 = new Line();
+            Line line2 = new Line();
+            line1.LengthOfLine(1, 2, 3, 4);
+            line2.LengthOfLine(1, 2, 3, 4);
+        }
     }
 }
