d7439bba7ea4dda6912d62ed1ad4745d32a2ec91
Madhuri <harshupatil0301@gmail>
2021-05-28 11:14:27 -0700

    Lines Are Compared

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -9,16 +9,21 @@ namespace LineComparison
             Console.WriteLine("Welcome To Line Comparison Program");
             Line line1 = new Line();
             Line line2 = new Line();
-            double a = line1.LengthOfLine(5, 7, 10, 12);
+            double a = line1.LengthOfLine(3, 7, 10, 12);
             double b = line2.LengthOfLine(5, 7, 10, 12);
-
-            if (a.Equals(b))
+          
+            double c = (a.CompareTo(b));
+            if (c == 0)
             {
                 Console.WriteLine("Lines Are Equal");
             }
-            else
+            else if (c == 1)
             {
-                Console.WriteLine("Lines Are not Equals");
+                Console.WriteLine("Line One Is Greater Than Line Two");
+            }
+            else 
+            {
+                Console.WriteLine("Line Two Is Greater Than Line One");
             }
         }
     }
